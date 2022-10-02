c85a343ae0ff990afed2fd2acd988f6b44f55f65
Abubakar Ummar <Haywaya@Abubakars-MacBook-Pro.local>
2022-10-02 15:47:18 +0000

    Import DateTime from luxon.js

--- src/modules/Function.js
+++ src/modules/Function.js
@@ -1,8 +1,9 @@
 import { timeAndDate } from './Variables.js';
+import { DateTime } from './luxon.js';
 
 function dateAndTime() {
   setInterval(() => {
-    const date = new Date().toUTCString();
+    const date = DateTime.now().toLocaleString(DateTime.DATETIME_FULL_WITH_SECONDS);
     timeAndDate.innerHTML = date;
   }, 0);
 }
